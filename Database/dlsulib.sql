86d053e082369990df2b0e7f77a0a7af5e1ab9e6
sygted <eugene_lim@dlsu.edu.ph>
2017-07-14 21:30:43 +0800

    db

--- Database/dlsulib.sql
+++ Database/dlsulib.sql
@@ -70,8 +70,32 @@ CREATE TABLE `catalog` (
 
 LOCK TABLES `catalog` WRITE;
 /*!40000 ALTER TABLE `catalog` DISABLE KEYS */;
-INSERT INTO `catalog` VALUES (1,'Challengers of the Unknown','Ron Goulart',1977,'	Dell','United States','Magazine','4','Gangster'),(2,'A Life in the Streets','Eugene Lim',2017,'Ted Publishing House','Quezon City','Magazine ','4','Blood, Sweat, Tears'),(3,'Buhay ng Gangsta','Smugglaz',2016,'187 Mobstaz','Muntinlupa City','Book','4','Streets'),(4,'Blithe Spirit','	Noël Coward',1941,'Noel','Sweden','Magazine','4','Spirit'),(5,'The Lord of The Rings','J. R. R. Tolkien',1955,'Allen & Unwin','New Zeland','Book','4','Rings, Lord'),(6,'Kingdom Come','Elliot S. Maggin, with Mark Waid, Alex Ross',1998,'Warner Books','New York','Book','4','Kingdom'),(7,'Three Little Bears','Yemear ',2017,'Ming Publishing House','Laguna ','Book','4','Bears, Ming, Ling, Mong, Long'),(8,'The Adventures of Superman','George Lowther',1979,'Kassel Books ','North America','Book','4','Superman'),(9,'Batman & Robin','Michael Jan Friedman',1997,'Warner Books','United States','Book','4','Batman, Robin, Bat'),(10,'Steel','Dean Wesley Smith',1997,'Tor Books','Sweden','Book','4','Man of Steel, Steel'),(17,'Secure Internet of Things','	Dr.	Thiemo	Voigt',2017,'SICS Swedish Lab','Stockholm','Thesis','4','Internet of Things, Internet');
+INSERT INTO `catalog` VALUES (1,'Challengers of the Unknown','Ron Goulart',1977,'	Dell','United States','2','4','Gangster'),(2,'A Life in the Streets','Eugene Lim',2017,'Ted Publishing House','Quezon City','2 ','4','Blood, Sweat, Tears'),(3,'Buhay ng Gangsta','Smugglaz',2016,'187 Mobstaz','Muntinlupa City','1','4','Streets'),(4,'Blithe Spirit','	Noël Coward',1941,'Noel','Sweden','2','4','Spirit'),(5,'The Lord of The Rings','J. R. R. Tolkien',1955,'Allen & Unwin','New Zeland','1','4','Rings, Lord'),(6,'Kingdom Come','Elliot S. Maggin, with Mark Waid, Alex Ross',1998,'Warner Books','New York','1','4','Kingdom'),(7,'Three Little Bears','Yemear ',2017,'Ming Publishing House','Laguna ','1','4','Bears, Ming, Ling, Mong, Long'),(8,'The Adventures of Superman','George Lowther',1979,'Kassel Books ','North America','1','4','Superman'),(9,'Batman & Robin','Michael Jan Friedman',1997,'Warner Books','United States','1','4','Batman, Robin, Bat'),(10,'Steel','Dean Wesley Smith',1997,'Tor Books','Sweden','1','4','Man of Steel, Steel'),(17,'Secure Internet of Things','	Dr.	Thiemo	Voigt',2017,'SICS Swedish Lab','Stockholm','3','4','Internet of Things, Internet');
 /*!40000 ALTER TABLE `catalog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `catalogtype`
+--
+
+DROP TABLE IF EXISTS `catalogtype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `catalogtype` (
+  `catalogid` int(11) NOT NULL AUTO_INCREMENT,
+  `catalogtype` varchar(45) NOT NULL,
+  PRIMARY KEY (`catalogid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `catalogtype`
+--
+
+LOCK TABLES `catalogtype` WRITE;
+/*!40000 ALTER TABLE `catalogtype` DISABLE KEYS */;
+INSERT INTO `catalogtype` VALUES (1,'Book'),(2,'Magazine'),(3,'Thesis');
+/*!40000 ALTER TABLE `catalogtype` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -140,7 +164,7 @@ CREATE TABLE `reservation` (
   `dateReserved` varchar(45) NOT NULL,
   `statusId` int(11) NOT NULL,
   PRIMARY KEY (`reserveId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +173,7 @@ CREATE TABLE `reservation` (
 
 LOCK TABLES `reservation` WRITE;
 /*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` VALUES (1,1,1,'2017-07-06 05:52:16',1);
+INSERT INTO `reservation` VALUES (1,1,1,'2017-07-06 05:52:16',1),(2,2,2,'2017-07-10 06:00:16',1);
 /*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,7 +191,7 @@ CREATE TABLE `review` (
   `review` varchar(200) NOT NULL,
   `dateReviewed` datetime DEFAULT NULL,
   PRIMARY KEY (`reviewId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,7 +200,7 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
-INSERT INTO `review` VALUES (1,1,1,'\"Very nice read\"',NULL),(2,2,2,'\"Wow\"',NULL);
+INSERT INTO `review` VALUES (1,1,1,'\"Very nice read\"','2017-07-10 00:00:00'),(2,2,2,'\"Wow\"','2017-07-13 00:00:00'),(3,3,3,'\"Nice Book!\"','2017-07-12 00:00:00');
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -194,7 +218,7 @@ CREATE TABLE `roomtimeslot` (
   `endTimeslot` varchar(45) NOT NULL,
   `roomType` varchar(45) NOT NULL,
   PRIMARY KEY (`timeslotId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -203,7 +227,7 @@ CREATE TABLE `roomtimeslot` (
 
 LOCK TABLES `roomtimeslot` WRITE;
 /*!40000 ALTER TABLE `roomtimeslot` DISABLE KEYS */;
-INSERT INTO `roomtimeslot` VALUES (1,'3','2017-07-10 05:52:16','2017-07-10 07:52:16','Leonardo'),(2,'3','2017-07-10 10:52:16','2017-07-10 12:52:16','MichaelAngelo');
+INSERT INTO `roomtimeslot` VALUES (1,'3','2017-07-10 05:52:16','2017-07-10 07:52:16','Leonardo'),(2,'3','2017-07-10 10:52:16','2017-07-10 12:52:16','MichaelAngelo'),(3,'3','2017-07-13 09:52:16','2017-07-13 12:52:16','Donatello');
 /*!40000 ALTER TABLE `roomtimeslot` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -256,7 +280,7 @@ CREATE TABLE `user` (
   `SQID` int(11) NOT NULL,
   `SQAnser` varchar(250) NOT NULL,
   PRIMARY KEY (`userId`,`firstname`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -265,8 +289,32 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (2,'teddyboi','test123','teddy_lim@gmail.com','Ted','Go','Lim','student',NULL,'1',19,5,1996,11342498,1,'Dog'),(3,'hazel_anne','test456','hazel_anne@gmail.com','Hazel','Legaspi','Brosas','studen',NULL,'1',8,11,1997,11425598,1,'Pig'),(4,'randolph_yu','randy_santiago','randolph_yu@gmail.com','Randolph','Lim','Yu','student',NULL,'-1',28,11,1997,11433523,1,'Dinosaur');
+INSERT INTO `user` VALUES (2,'teddyboi','test123','teddy_lim@gmail.com','Ted','Go','Lim','student',NULL,'1',19,5,1996,11342498,1,'Dog'),(3,'hazel_anne','test456','hazel_anne@gmail.com','Hazel','Legaspi','Brosas','studen',NULL,'1',8,11,1997,11425598,1,'Pig'),(4,'randolph_yu','randy_santiago','randolph_yu@gmail.com','Randolph','Lim','Yu','student',NULL,'-1',28,11,1997,11433523,1,'Dinosaur'),(5,'den_silva','denise_123','denise_silva@gmail.com','Denise Anne','Restua','Silva','student',NULL,'1',1,4,1997,11421234,2,'Grahams');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `userroles`
+--
+
+DROP TABLE IF EXISTS `userroles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userroles` (
+  `roleid` int(11) NOT NULL AUTO_INCREMENT,
+  `roletype` varchar(45) NOT NULL,
+  PRIMARY KEY (`roleid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userroles`
+--
+
+LOCK TABLES `userroles` WRITE;
+/*!40000 ALTER TABLE `userroles` DISABLE KEYS */;
+INSERT INTO `userroles` VALUES (1,'Student'),(2,'Library Manager'),(3,'Library Staff'),(4,'Administrator'),(5,'Employee'),(6,'Roles');
+/*!40000 ALTER TABLE `userroles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -278,4 +326,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-14  3:36:35
+-- Dump completed on 2017-07-14 21:21:43
