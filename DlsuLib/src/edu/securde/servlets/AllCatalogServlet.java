16bbdb553dc877ba5443ef102f5b4f66b781106a
yu-randolph <rmy1128@gmail.com>
2017-08-10 19:55:58 +0800

    fixed merge conflicts

--- DlsuLib/src/edu/securde/servlets/AllCatalogServlet.java
+++ DlsuLib/src/edu/securde/servlets/AllCatalogServlet.java
@@ -9,20 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import edu.securde.beans.User;
-=======
-=======
->>>>>>> 8e44629951476a6c4358b627fe2818d71a7497dc
 import edu.securde.beans.Catalog;
 import edu.securde.beans.User;
 import edu.securde.manager.CatalogManager;
 import edu.securde.manager.UserManager;
-<<<<<<< HEAD
->>>>>>> 8e44629951476a6c4358b627fe2818d71a7497dc
-=======
->>>>>>> 8e44629951476a6c4358b627fe2818d71a7497dc
 
 /**
  * Servlet implementation class AllCatalogServlet
@@ -44,14 +34,8 @@ public class AllCatalogServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-<<<<<<< HEAD
 		User user = (User) request.getAttribute("user");
 		System.out.println(user.getFirstname());
-=======
->>>>>>> 8e44629951476a6c4358b627fe2818d71a7497dc
-=======
->>>>>>> 8e44629951476a6c4358b627fe2818d71a7497dc
 	}
 
 	/**
