f23d3b0e8b31ba12aeb18150f8612bbc141d07ef
sygted <eugene_lim@dlsu.edu.ph>
2017-08-14 23:18:12 +0800

    Hash password and Answer

--- DlsuLib/src/edu/securde/manager/Hash.java
+++ DlsuLib/src/edu/securde/manager/Hash.java
@@ -7,8 +7,9 @@ import org.apache.shiro.crypto.hash.Sha256Hash;
 public class Hash {
 	private String hashSalt, hashString;
 	
-	public Hash(String value) {
-		hash(value);
+	public Hash(String value, String saltvalue) {
+		String hash = "";
+		hash = getHash(value,saltvalue);
 	}
 	
 	public String getHashSalt() {
@@ -19,13 +20,11 @@ public class Hash {
 		return hashString;
 	}
 
-	public void hash(String value) {
-		RandomNumberGenerator rng = new SecureRandomNumberGenerator();
-		String salt = rng.nextBytes().toString();
-		String hashedPasswordBase64 = new Sha256Hash(value, salt, 1024).toBase64();
-		
-		System.out.println("This is the hash salt : " + salt.toString());
+	public static String getHash(String value, String saltvalue) {
+		String hashedPasswordBase64 = new Sha256Hash(value, saltvalue, 1024).toBase64();
 		System.out.println("This is the hashed password : " + hashedPasswordBase64);
+		
+		return hashedPasswordBase64;
 	}
-
+	
 }
